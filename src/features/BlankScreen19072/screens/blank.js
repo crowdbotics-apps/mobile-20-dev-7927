3ad263686dc469990d6264d99bc714958c6d2cf9
Crowdbotics <team@crowdbotics.com>
2020-07-20 08:53:59 +0000

    Update components

--- src/features/BlankScreen19072/screens/blank.js
+++ src/features/BlankScreen19072/screens/blank.js
@@ -24,7 +24,6 @@ export class _Blank extends React.Component {
 
   state = {
     Input_3: "sfawefsdfsf",
-    Input_7: "",
     Datepicker_9: new Date("07/20/2020"),
     Toggle_11: true,
     Radio_15: true,
@@ -99,43 +98,6 @@ export class _Blank extends React.Component {
         }}
         value={this.state.Input_3}
         onChangeText={nextValue => this.setState({ Input_3: nextValue })}
-      />
-      <Input
-        placeholder="Sample text input placeholder"
-        editable={true}
-        multiline={true}
-        textStyle={{ height: 100, fontSize: 20, color: "#000000" }}
-        style={{
-          width: "100%",
-          marginLeft: 0,
-          marginRight: 0,
-          marginTop: 5,
-          marginBottom: 5,
-          paddingLeft: 5,
-          paddingRight: 5,
-          paddingTop: 5,
-          paddingBottom: 5,
-          overflow: "visible",
-          textAlign: "left",
-          verticalAlign: "baseline",
-          fontSize: 20,
-          color: "#000000",
-          backgroundColor: "#ffffff",
-          fontStyle: "normal",
-          fontWeight: "normal",
-          borderColor: "#000000",
-          borderStyle: "solid",
-          borderWidth: 0,
-          borderLeftWidth: 0,
-          borderRightWidth: 0,
-          borderTopWidth: 0,
-          borderBottomWidth: 0,
-          borderRadius: 0,
-          textShadow: "0pt 0pt 0pt #000000",
-          textDecoration: "none"
-        }}
-        value={this.state.Input_7}
-        onChangeText={nextValue => this.setState({ Input_7: nextValue })}
       />
       <Datepicker
         style={{
