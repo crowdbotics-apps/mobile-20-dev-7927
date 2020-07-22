d6b6595eecf68d3ca235e8e9601bfb80b71b1aba
Crowdbotics <team@crowdbotics.com>
2020-07-22 11:22:39 +0000

    Update components

--- src/features/BlankScreen69233/screens/blank.js
+++ src/features/BlankScreen69233/screens/blank.js
@@ -100,34 +100,6 @@ export class _Blank extends React.Component {
         date={this.state.Datepicker_3}
         onSelect={nextValue => this.setState({ Datepicker_3: nextValue })}
       />
-      <Slider
-        value={50}
-        minimumValue={0}
-        maximumValue={100}
-        step={1}
-        disabled={false}
-        style={{
-          marginLeft: 0,
-          marginRight: 0,
-          marginTop: 5,
-          marginBottom: 5,
-          paddingLeft: 5,
-          paddingRight: 5,
-          paddingTop: 5,
-          paddingBottom: 5,
-          overflow: "visible",
-          textAlign: "left",
-          verticalAlign: "baseline",
-          borderColor: "#000000",
-          borderStyle: "solid",
-          borderWidth: 0,
-          borderLeftWidth: 0,
-          borderRightWidth: 0,
-          borderTopWidth: 0,
-          borderBottomWidth: 0,
-          borderRadius: 0
-        }}
-      />
       <Image
         resizeMode="cover"
         style={{
