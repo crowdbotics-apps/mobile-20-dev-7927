2c24575ee638b298cd3033956d368e04164877a9
Crowdbotics <team@crowdbotics.com>
2020-07-22 11:44:26 +0000

    Update components

--- src/features/CopyOfBlankScreen49231/screens/blank.js
+++ src/features/CopyOfBlankScreen49231/screens/blank.js
@@ -66,39 +66,6 @@ export class _Blank extends React.Component {
         backgroundSize: "auto"
       }}
     >
-      <Text
-        style={{
-          marginLeft: 0,
-          marginRight: 0,
-          marginTop: 5,
-          marginBottom: 5,
-          paddingLeft: 5,
-          paddingRight: 5,
-          paddingTop: 5,
-          paddingBottom: 5,
-          overflow: "visible",
-          textAlign: "left",
-          verticalAlign: "baseline",
-          fontSize: 20,
-          color: "#000000",
-          backgroundColor: "#ffffff",
-          fontStyle: "normal",
-          fontWeight: "normal",
-          borderColor: "#000000",
-          borderStyle: "solid",
-          borderWidth: 0,
-          borderLeftWidth: 0,
-          borderRightWidth: 0,
-          borderTopWidth: 0,
-          borderBottomWidth: 0,
-          borderRadius: 0,
-          textShadow: "0pt 0pt 0pt #000000",
-          textDecoration: "none",
-          textTransform: "none"
-        }}
-      >
-        Sample text content
-      </Text>
       <Datepicker
         style={{
           width: "100%",
