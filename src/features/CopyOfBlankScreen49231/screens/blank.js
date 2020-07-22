2c12c5fd6f545edd5137d33f77db1d134b444dd1
Crowdbotics <team@crowdbotics.com>
2020-07-22 12:21:55 +0000

    Update components

--- src/features/CopyOfBlankScreen49231/screens/blank.js
+++ src/features/CopyOfBlankScreen49231/screens/blank.js
@@ -23,7 +23,6 @@ export class _Blank extends React.Component {
   }
 
   state = {
-    Input_29: "",
     Input_5: "",
     Input_13: "",
     Datepicker_17: new Date("07/22/2020"),
@@ -69,43 +68,6 @@ export class _Blank extends React.Component {
         backgroundSize: "auto"
       }}
     >
-      <Input
-        placeholder="Sample text input placeholder"
-        editable={true}
-        textStyle={{ fontSize: 20, color: "#000000" }}
-        style={{
-          width: "100%",
-          marginLeft: 0,
-          marginRight: 0,
-          marginTop: 5,
-          marginBottom: 5,
-          paddingLeft: 5,
-          paddingRight: 5,
-          paddingTop: 5,
-          paddingBottom: 5,
-          overflow: "visible",
-          textAlign: "left",
-          verticalAlign: "baseline",
-          fontSize: 20,
-          color: "#000000",
-          backgroundColor: "#ffffff",
-          fontStyle: "normal",
-          fontWeight: "normal",
-          borderColor: "#000000",
-          borderStyle: "solid",
-          borderWidth: 0,
-          borderLeftWidth: 0,
-          borderRightWidth: 0,
-          borderTopWidth: 0,
-          borderBottomWidth: 0,
-          borderRadius: 0,
-          textShadow: "0pt 0pt 0pt #000000",
-          textDecoration: "none",
-          textTransform: "none"
-        }}
-        value={this.state.Input_29}
-        onChangeText={nextValue => this.setState({ Input_29: nextValue })}
-      />
       <Input
         placeholder="Sample text input placeholder"
         editable={true}
