7f7a81e748354de2aafb3e9b6374877b0e300983
Crowdbotics <team@crowdbotics.com>
2020-07-22 10:54:19 +0000

    Update components

--- src/features/CopyOfBlankScreen49231/screens/blank.js
+++ src/features/CopyOfBlankScreen49231/screens/blank.js
@@ -1886,41 +1886,6 @@ export class _Blank extends React.Component {
         value={this.state.Input_7}
         onChangeText={nextValue => this.setState({ Input_7: nextValue })}
       />
-      <Icon
-        iconFont="Eva Design Icons"
-        name="github"
-        width={20}
-        height={20}
-        style={{
-          marginLeft: 0,
-          marginRight: 0,
-          marginTop: 5,
-          marginBottom: 5,
-          paddingLeft: 5,
-          paddingRight: 5,
-          paddingTop: 5,
-          paddingBottom: 5,
-          overflow: "visible",
-          textAlign: "left",
-          verticalAlign: "baseline",
-          borderColor: "#000000",
-          borderStyle: "solid",
-          borderWidth: 0,
-          borderLeftWidth: 0,
-          borderRightWidth: 0,
-          borderTopWidth: 0,
-          borderBottomWidth: 0,
-          borderRadius: 0,
-          fontSize: 20,
-          color: "#000000",
-          backgroundColor: "#ffffff",
-          fontStyle: "normal",
-          fontWeight: "normal",
-          textShadow: "0pt 0pt 0pt #000000",
-          textDecoration: "none",
-          textTransform: "none"
-        }}
-      />
     </View>
   )
 }
