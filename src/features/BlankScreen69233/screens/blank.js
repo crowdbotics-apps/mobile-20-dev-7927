eebf5e3fc894bf23625e5ab73e13feb251d863cb
Crowdbotics <team@crowdbotics.com>
2020-07-22 11:20:43 +0000

    Update components

--- src/features/BlankScreen69233/screens/blank.js
+++ src/features/BlankScreen69233/screens/blank.js
@@ -22,12 +22,7 @@ export class _Blank extends React.Component {
     }
   }
 
-  state = {
-    Datepicker_9: new Date("07/22/2020"),
-    Toggle_3: true,
-    Radio_3: true,
-    Input_5: ""
-  }
+  state = { Toggle_3: true, Radio_3: true, Input_5: "" }
 
   render = () => (
     <View
@@ -66,40 +61,6 @@ export class _Blank extends React.Component {
         backgroundSize: "auto"
       }}
     >
-      <Datepicker
-        style={{
-          width: "100%",
-          marginLeft: 0,
-          marginRight: 0,
-          marginTop: 5,
-          marginBottom: 5,
-          paddingLeft: 5,
-          paddingRight: 5,
-          paddingTop: 5,
-          paddingBottom: 5,
-          overflow: "visible",
-          textAlign: "left",
-          verticalAlign: "baseline",
-          fontSize: 20,
-          color: "#000000",
-          backgroundColor: "#ffffff",
-          fontStyle: "normal",
-          fontWeight: "normal",
-          borderColor: "#000000",
-          borderStyle: "solid",
-          borderWidth: 0,
-          borderLeftWidth: 0,
-          borderRightWidth: 0,
-          borderTopWidth: 0,
-          borderBottomWidth: 0,
-          borderRadius: 0,
-          textShadow: "0pt 0pt 0pt #000000",
-          textDecoration: "none",
-          textTransform: "none"
-        }}
-        date={this.state.Datepicker_9}
-        onSelect={nextValue => this.setState({ Datepicker_9: nextValue })}
-      />
       <Slider
         value={50}
         minimumValue={0}
